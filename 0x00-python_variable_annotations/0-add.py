96958cb158f950cafad9992df0bc51f3e566f51e
= <=>
2024-08-21 10:29:27 +0000

    Basic annotations-concat

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 '''
-Implements a function called addd
+Implements a function called add
 '''
+
 
 def add(a: float, b: float) -> float:
     ''' Adds two numbers '''
-    return a + b;
+    return a + b
