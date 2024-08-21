2484046539d68c929670a2e164e6feccc098e06d
= <=>
2024-08-21 10:41:30 +0000

    Basic annotations - to string

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -4,4 +4,5 @@ import math
 
 
 def floor(n: float) -> int:
+    ''' Returns the floor of a number '''
     return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' Implements a function that returns a str repr. of a float '''
+
+
+def to_str(n: float) -> str:
+    ''' Returns a string representation of a number '''
+    return str(n)
