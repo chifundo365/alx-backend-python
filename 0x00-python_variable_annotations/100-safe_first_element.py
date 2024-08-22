d5a1daf62041c67bfeb561f521beb28e0e2531e4
= <=>
2024-08-22 14:00:23 +0000

    Duck typing - first element of a sequence

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -3,7 +3,7 @@
 from typing import Sequence, Union, Any
 
 
-def safe_first_element(lst: Sequence) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     '''
     Returns the first item of an iterable or None if lst id not iterable
     '''
