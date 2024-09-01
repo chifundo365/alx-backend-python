d1d481c5a49d78aacf91bc6832aef0415a4594bf
= <=>
2024-09-01 13:13:26 +0000

    Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,4 +6,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    ''' Correts 10 random values from another function '''
     return [i async for i in async_generator()]
