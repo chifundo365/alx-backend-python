a5e7bf8300450adc31972ef5a78895875e386d7a
= <=>
2024-09-01 12:38:12 +0000

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,6 +6,7 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator:
+    ''' Yields 10 random values after waiting for a second for each value '''
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
