7dc11fe8ea3958b84b77bfb4dcfa7b49b2f74e90
= <=>
2024-09-01 12:35:03 +0000

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' Implement an asyncronous coroutine '''
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator:
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
