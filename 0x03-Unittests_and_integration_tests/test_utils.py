931a80a3a78ff486d1948eb177d7ce0c106a3759
= <=>
2024-09-20 13:09:29 +0000

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,18 +2,25 @@
 """ Unit Testing a function """
 import unittest
 from parameterized import parameterized
+from typing import Mapping, Sequence, Union, Dict, Any
 from utils import access_nested_map
 
 
 class TestAccsesNestedMap(unittest.TestCase):
     """ Run dedicated tests """
-    @parameterised.expand(
+    @parameterized.expand(
             [
 
-                (nested_map={"a": 1}, path=("a",), 1)
-                (nested_map={"a": {"b": 2}}, path=("a",), {"b": 2})
-                (nested_map={"a": {"b": 2}}, path=("a", "b"), 2)
+                ({"a": 1}, ("a",), 1),
+                ({"a": {"b": 2}}, ("a",), {"b": 2}),
+                ({"a": {"b": 2}}, ("a", "b"), 2)
             ])
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(
+            self, nested_map: Mapping, path: Sequence, expected: Any
+            ) -> None:
         """ tests with different paramiterised input """
-        assert_equal(access_nested_map(nested_map, path), expected)
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
