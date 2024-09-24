e3e7ab63c8de5990a2cf8ecd5881dd48e9d227d2
= <=>
2024-09-24 10:05:56 +0000

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 ''' Tests an API '''
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 import client
 
@@ -14,8 +14,8 @@ class TestGithubOrgClient(unittest.TestCase):
             [
                 ("google",),
                 ("abc",)
-            ]
-    )
+                ]
+            )
     @patch('client.get_json')
     def test_org(self, org, mock):
         ''' Self descriptive '''
@@ -23,6 +23,20 @@ class TestGithubOrgClient(unittest.TestCase):
         spec = client.GithubOrgClient(org)
         spec.org()
         mock.assert_called_once_with(endpoint)
+
+    @parameterized.expand([
+        ('random_url', {'repos_url': 'https://some_url.com'})
+        ])
+    def test_public_repos_url(self, name, result):
+        ''' Tests public_repos_url '''
+        with patch(
+                'client.GithubOrgClient.org',
+                new_callable=PropertyMock
+                ) as mock_org:
+            mock_org.return_value = result
+            res = client.GithubOrgClient(name)._public_repos_url
+            self.assertEqual(res, result.get('repos_url'))
+
 
 if __name__ == "__main__":
     unittest.main()
