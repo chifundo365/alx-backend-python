8e88e781d606e5f8526426ea31a6144a9bc0ea6f
= <=>
2024-09-24 11:09:40 +0000

    Parameterize

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -54,6 +54,14 @@ class TestGithubOrgClient(unittest.TestCase):
             mocked_public.assert_called_once()
             mock_get_json.assert_called_once()
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+        ])
+    def test_has_license(self, repo, license_key, expected_result):
+        result = client.GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected_result)
+
 
 if __name__ == "__main__":
     unittest.main()
